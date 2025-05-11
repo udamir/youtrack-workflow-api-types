2ce09f499be227c75067c58c4e37a38b99aa3a37
Damir <damir@yusipov.ru>
2025-05-11 22:22:50 +0300

    update exports

--- README.md
+++ README.md
@@ -5,5 +5,47 @@ TypeScript definitions for the JetBrains YouTrack Scripting API.
 ## Installation
 
 ```bash
-npm install --save-dev @types/jetbrains__youtrack-scripting-api
+npm install --save-dev youtrack-workflow-api-types
 ```
+
+## Usage
+
+Add `types/@jetbrains/youtrack-scripting-api/index.d.ts` to your project with following content:
+
+```typescript
+/**
+ * Type reference file for YouTrack Workflow API
+ */
+
+declare module '@jetbrains/youtrack-scripting-api' {
+  export * from 'youtrack-workflow-api-types/src';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/entities' {
+  export * from 'youtrack-workflow-api-types/src/entities';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/workflow' {
+  export * from 'youtrack-workflow-api-types/src/workflow';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/http' {
+  export * from 'youtrack-workflow-api-types/src/http';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/search' {
+  export * from 'youtrack-workflow-api-types/src/search';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/date-time' {
+  export * from 'youtrack-workflow-api-types/src/date-time';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/notifications' {
+  export * from 'youtrack-workflow-api-types/src/notifications';
+}
+
+declare module '@jetbrains/youtrack-scripting-api/strings' {
+  export * from 'youtrack-workflow-api-types/src/strings';
+}
+
--- src/entities/index.d.ts
+++ src/entities/index.d.ts
@@ -4,69 +4,20 @@
  */
 
 // Import all modules
-import * as workitem from './workitem';
-import * as helpdesk from './helpdesk';
-import * as articles from './articles';
-import * as project from './project';
-import * as search from './search';
-import * as issue from './issue';
-import * as agile from './agile';
-import * as field from './field';
-import * as user from './user';
+export * from './workitem';
+export * from './helpdesk';
+export * from './articles';
+export * from './project';
+export * from './search';
+export * from './issue';
+export * from './agile';
+export * from './field';
+export * from './user';
+export * from './core';
+export * from './vcs';
+export * from './tag';
+
 import * as core from './core';
-import * as vcs from './vcs';
-import * as tag from './tag';
 
-
-// Re-export all types from workitem module
-export import IssueWorkItem = workitem.IssueWorkItem;
-export import WorkItemType = workitem.WorkItemType;
-
-// Re-export all types from helpdesk module
-export import Calendar = helpdesk.Calendar;
-export import Channel = helpdesk.Channel;
-
-// Re-export all types from articles module
-export import BaseArticle = articles.BaseArticle;
-
-// Re-export all types from project module
-export import Project = project.Project;
-
-// Re-export all types from search module
-export import WatchFolder = search.WatchFolder;
-export import SavedQuery = search.SavedQuery;
-
-// Re-export all types from issue module
-export import Issue = issue.Issue;
-export import IssueComment = issue.IssueComment;
-export import IssueAttachment = issue.IssueAttachment;
-export import IssueRuleContext = issue.IssueRuleContext;
-export import IssueCommentActionContext = issue.IssueCommentActionContext;
-export import IssueAttachmentActionContext = issue.IssueAttachmentActionContext;
-export import BaseComment = issue.BaseComment;
-
-// Re-export all types from agile module
-export import Sprint = agile.Sprint;
-export import Agile = agile.Agile;
-
-// Re-export all types from field module
-export import Fields = field.Fields;
-
-// Re-export all types from user module
-export import User = user.User;
-export import UserGroup = user.UserGroup;
-export import ProjectTeam = user.ProjectTeam;
-
-// Re-export all types from core module
-export import BaseEntity = core.BaseEntity;
-// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+// biome-ignore lint/suspicious/noShadowRestrictedNames: 
 export import Set = core.YTSet;
-export import Requirements = core.Requirements;
-export import PersistentFile = core.PersistentFile;
-
-// Re-export all types from vcs module
-export import PullRequest = vcs.PullRequest;
-export import VcsChange = vcs.VcsChange;
-
-// Re-export all types from tag module
-export import Tag = tag.Tag;
